4d8bd2b38167f174abd61d366f55a38ccc942adb
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-10-22 12:33:37 +0200

    Add `webextension-polyfill` and it types

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,7 @@
+import browser from "webextension-polyfill";
+
 console.log(`ContainerTabVault; start => ${Date.now()}`);
 
+browser.tabs.onCreated.addListener(async (tab: browser.Tabs.Tab) => {
+    console.log(tab);
+})
