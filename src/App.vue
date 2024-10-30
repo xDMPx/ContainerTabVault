e8768d6d9145ed155563552cc9c286ead5b01456
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-10-30 10:58:12 +0100

    Add SVG icons for edit and delete buttons

--- src/App.vue
+++ src/App.vue
@@ -74,10 +74,25 @@ onMounted(onMountedHook);
 <template>
     <div class="w-full h-full table p-2 space-y-2">
         <div v-for="name in savedNames">
-            <div v-if="editWorkspace !== name">
+            <div class="flex" v-if="editWorkspace !== name">
                 <button class="btn btn-link btn-xs" @click="onWorkspaceNameClick(name)">{{ name }}</button>
-                <button class="btn btn-ghost btn-xs" @click="onDeleteWorkspaceClick(name)">X</button>
-                <button class="btn btn-ghost btn-xs" @click="onEditWorkspaceNameClick(name)">E</button>
+                <button class="btn btn-ghost btn-xs" @click="onEditWorkspaceNameClick(name)">
+                    <!-- https://github.com/google/material-design-icons#license -->
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
+                        fill="#e8eaed">
+                        <path
+                            d="M216-216h51l375-375-51-51-375 375v51Zm-72 72v-153l498-498q11-11 23.84-16 12.83-5 27-5 14.16 0 27.16 5t24 16l51 51q11 11 16 24t5 26.54q0 14.45-5.02 27.54T795-642L297-144H144Zm600-549-51-51 51 51Zm-127.95 76.95L591-642l51 51-25.95-25.05Z" />
+                    </svg>
+                </button>
+                <button class="btn btn-ghost btn-xs" @click="onDeleteWorkspaceClick(name)">
+                    <!-- https://github.com/google/material-design-icons#license -->
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px"
+                        fill="#e8eaed">
+                        <path
+                            d="M312-144q-29.7 0-50.85-21.15Q240-186.3 240-216v-480h-48v-72h192v-48h192v48h192v72h-48v479.57Q720-186 698.85-165T648-144H312Zm336-552H312v480h336v-480ZM384-288h72v-336h-72v336Zm120 0h72v-336h-72v336ZM312-696v480-480Z" />
+                    </svg>
+                </button>
+
             </div>
             <div v-else>
                 <input ref="workspaceNameInput" type="text" v-model="newWorkspaceName"
